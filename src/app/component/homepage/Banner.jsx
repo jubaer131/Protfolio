cd83503abc48dc1fd1293f9c6b9313cf5117e3b1
juber131 <jubayerahmad131@gmail.com>
2024-09-27 12:22:57 +0600

    add location

--- src/app/component/homepage/Banner.jsx
+++ src/app/component/homepage/Banner.jsx
@@ -3,12 +3,14 @@ import Link from 'next/link';
 import React, { useEffect } from 'react';
 import Image from 'next/image';
 import Typewriter from 'typewriter-effect/dist/core';
+import { TiLocation } from "react-icons/ti";
+
 const Banner = () => {
 
 
     useEffect(() => {
         const typewriter = new Typewriter('#typewriter', {
-            strings: ['Hi, I am Jubaer Ahmed', 'Frontend Web Developer'],
+            strings: ['Hi, I am Jubaer Ahmed', ' Front-End Web Developer'],
             autoStart: true,
             loop: true,
         });
@@ -19,23 +21,24 @@ const Banner = () => {
 
         <section className="container mx-auto  shadow-sm rounded-xl">
             <div className=" flex flex-col-reverse max-sm:gap-10 justify-center items-center  sm:py-12 lg:py-24 lg:flex-row">
-                <div className="flex flex-col justify-center items-center rounded-sm lg:w-8/12 space-y-2 lg:space-y-4">
+                <div className="flex flex-col justify-center items-center rounded-sm lg:w-8/12 space-y-2 lg:space-y-3">
                     <h1 id="typewriter" className='md:text-4xl text-2xl text-[#3193ee] font-bold text-start w-10/12 lg:w-11/12 lg:px-8'>
                         {/* Hi, I am Jubaer Ahmed */}
                     </h1>
-                    <h1 className='text-3xl text-white font-semibold text-start w-10/12 lg:w-11/12 lg:px-8'>
+                    <h1 className='text-[18px] text-white font-medium text-start w-10/12 lg:w-11/12 lg:px-8'>
 
 
                     </h1>
+                    <h1 className='text-xl text-white font-semibold text-start w-10/12 lg:w-11/12 lg:px-8 flex items-center justify-start gap-2'><TiLocation className='text-green-500 text-2xl' /> Form Bangladesh</h1>
                     <h1 className="text-[18px] text-white lg:text-xl font-normal lg:w-11/12 px-8 leading-none sm:text-4xl lg:text-left">
-                        🚀 As a passionate and experienced Frontend Web Developer , I thrive on creating beautiful and interactive web applications that delight users. My journey in web development has been driven by a love for design and a commitment to crafting seamless user experiences.
+                        💻 As a passionate and experienced Frontend Web Developer , I thrive on creating beautiful and interactive web applications that delight users. My journey in web development has been driven by a love for design and a commitment to crafting seamless user experiences.
                     </h1>
 
                     <div className="flex flex-col px-8 space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start lg:w-11/12">
 
 
 
-                        <Link href="https://drive.google.com/uc?export=download&id=1a6baPLekBMVioJ4ofZ-KPlI6uu8ZK79J
+                        <Link href="https://drive.google.com/uc?export=download&id=1BpIAAD5Ejl2Qi1paFoIo5WI5l_uU0sdy
 ">
 
                             <button download="MyResume.pdf" class="mt-5 relative inline-flex items-center justify-center px-6 py-3 text-lg font-medium tracking-tighter text-white bg-gray-800 rounded-md group">
